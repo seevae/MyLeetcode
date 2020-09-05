6467162fe0b1bfc91dc609df529baacbf17cf8a4
zhangzhaoqi <906435177@qq.com>
2020-09-05 18:35:30 +0800

    ++

--- exerciseBook/src/zhaoqi/链表.java
+++ exerciseBook/src/zhaoqi/链表.java
@@ -3,10 +3,6 @@
 输入：head = [4,5,1,9], node = 5
 输出：[4,1,9]
 解释：给定你链表中值为 5 的第二个节点，那么在调用了你的函数之后，该链表应变为 4 -> 1 -> 9.
-
-来源：力扣（LeetCode）
-链接：https://leetcode-cn.com/problems/delete-node-in-a-linked-list
-著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 */
 /**
  * Definition for singly-linked list.
@@ -34,9 +30,6 @@ class Solution {
 
 输入： 1->2->3->4->5 和 k = 2
 输出： 4
-
-来源：力扣（LeetCode）
-链接：https://leetcode-cn.com/problems/kth-node-from-end-of-list-lcci
 **/
 /**
  * Definition for singly-linked list.
@@ -150,6 +143,7 @@ class Solution {
  * }
  */
 class Solution {
+    //新建一个头节点，然后两条链表进行大小比较，小的就串在新的链表之后，最后返回头节点后的第一个节点
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
         ListNode newhead = new ListNode();
         ListNode cur = newhead;
@@ -175,5 +169,143 @@ class Solution {
         }
 
         return newhead.next;
+    }
+}
+
+/**
+删除排序链表中的重复元素
+给定一个排序链表，删除所有重复的元素，使得每个元素只出现一次。
+
+示例 :
+输入: 1->1->2->3->3
+输出: 1->2->3
+**/
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode cur = head;
+
+        while(cur != null && cur.next != null){
+            if(cur.val == cur.next.val){
+                cur.next = cur.next.next;
+            }else{
+                cur = cur.next;
+            }
+        }
+        return head;
+    }
+}
+
+/**
+给定一个链表，判断链表中是否有环。
+
+为了表示给定链表中的环，我们使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。 如果 pos 是 -1，则在该链表中没有环。
+
+ 
+
+示例 1：
+
+输入：head = [3,2,0,-4], pos = 1
+输出：true
+解释：链表中有一个环，其尾部连接到第二个节点。
+**/
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    //快慢指针判断是否有环，注意while中的条件必须判断快指针的next是否为空
+    public boolean hasCycle(ListNode head) {
+        ListNode f = head;
+        ListNode s = head;
+
+        while(f != null && s != null && f.next != null){
+            f = f.next.next;
+            s = s.next;
+            if(f == s){
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
+//编写一个程序，找到两个单链表相交的起始节点。
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    //方法一：计算出两个链表的长度差，让长的先走出差值，然后两个再一起走
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        int lenA = getLength(headA);
+        int lenB = getLength(headB);  
+        ListNode l = null;
+        ListNode s = null;
+        int distance = lenA - lenB;
+
+        if (distance > 0){
+            l = headA;
+            s = headB;
+        }else{
+            distance = lenB -lenA;
+            l = headB;
+            s = headA;
+        }
+        for(int i=0;i<distance;i++){
+            l = l.next;
+        }
+        while(l != s){
+           l = l.next;
+           s = s.next;
+        }
+        ListNode r  = l;
+        return r;
+    }
+
+    public int getLength(ListNode head){
+        ListNode cur = head;
+        int len = 0;
+        while(cur != null){
+            len ++;
+            cur = cur.next;
+        }
+        return len;
+    }
+}
+//方法二：两个链表从头开始一起走，走到末尾时分别从对方链表头再开始走直到相遇
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode curA = headA;
+        ListNode curB = headB;
+
+        while(curA != curB){
+            curA = curA == null ? headB : curA.next;
+            curB = curB == null ? headA : curB.next;
+        }
+        return curA;
     }
 }
