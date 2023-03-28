f43444a4904c7797cf81f2260152bbd233f487b6
seevae <39720295+seevae@users.noreply.github.com>
2023-03-28 20:55:10 +0800

    Create LinkedDemo.java

---
+++ exerciseBook/src/zhaoqi/linkedList/LinkedDemo.java
@@ -0,0 +1,24 @@
+package zhaoqi.com.数据结构.链表;
+
+public class LinkedDemo implements LinkedOperator{
+    @Override
+    public Node addEnd(Node node) {
+        return null;
+    }
+
+    @Override
+    public Node addFront(Node node) {
+        return null;
+    }
+
+    @Override
+    public Node delete(Node node) {
+        return null;
+    }
+
+    @Override
+    public Node reverse(Node node) {
+        return null;
+    }
+}
+
