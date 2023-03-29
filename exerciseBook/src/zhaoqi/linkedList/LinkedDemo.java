a3f35edfb5e63ef13d6fb559819771f6d30b5536
seevae <39720295+seevae@users.noreply.github.com>
2023-03-29 21:01:21 +0800

    Update LinkedDemo.java

--- exerciseBook/src/zhaoqi/linkedList/LinkedDemo.java
+++ exerciseBook/src/zhaoqi/linkedList/LinkedDemo.java
@@ -1,14 +1,23 @@
 package zhaoqi.com.数据结构.链表;
 
-public class LinkedDemo implements LinkedOperator{
+public class LinkedDemo implements LinkedOperator {
     @Override
-    public Node addEnd(Node node) {
-        return null;
+    public Node addEnd(Node header, String newValue) {
+        Node p1 = header;
+        Node newNode = new Node(newValue);
+        while (p1.next != null){
+            p1 = p1.next;
+        }
+        p1.next = newNode;
+        return header;
     }
 
     @Override
-    public Node addFront(Node node) {
-        return null;
+    public Node addFront(Node header, String newValue) {
+        Node p = header;
+        Node newNode = new Node(newValue);
+        newNode.next = header;
+        return newNode;
     }
 
     @Override
@@ -19,6 +28,15 @@ public class LinkedDemo implements LinkedOperator{
     @Override
     public Node reverse(Node node) {
         return null;
+    }
+
+    @Override
+    public void printLinked(Node header) {
+        Node p = header;
+        while (p != null){
+            System.out.print(p.value + " ");
+            p = p.next;
+        }
     }
 }
 
